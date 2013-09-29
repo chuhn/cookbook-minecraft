98e7cb9eb67759b8ba42c23a9f707d0bf082d7e4
Greg Fitzgerald <greg@gregf.org>
2013-09-29 18:11:13 -0400

    Stick to the mojang instructions for starting up the server via runit.
    I added an additional attribute default['minecraft']['java-options']
    if anyone needs to pass any extra settings to java.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,8 @@ default['minecraft']['checksum']            = '7a1abdac5d412b7eebefd84030d40c159
 
 default['minecraft']['xms']                 = '128M'
 default['minecraft']['xmx']                 = '512M'
+# Additional options to be passed to java, for runit only
+default['minecraft']['java-options']        = ''
 default['minecraft']['init_style']          = 'mark2'
 
 default['minecraft']['banned-ips']          = []
