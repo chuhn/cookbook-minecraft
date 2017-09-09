2dd3d1fc44f7050164de6eb46819dc304f347e9d
Greg Fitzgerald <greg@gregf.org>
2017-09-09 17:35:04 -0400

    More cookstyle changes

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description       'Installs/Configures minecraft server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.6.0'
 name              'minecraft'
-issues_url         'https://github.com/gregf/chef-minecraft/issues'
+issues_url 'https://github.com/gregf/chef-minecraft/issues'
 source_url        'https://github.com/gregf/chef-minecraft/'
 chef_version      '13.2'
 
@@ -14,6 +14,6 @@ recipe 'minecraft', 'Installs and configures minecraft server.'
 depends 'java'
 #depends 'runit'
 
-%w[debian ubuntu centos].each do |os|
+%w(debian ubuntu centos).each do |os|
   supports os
 end
